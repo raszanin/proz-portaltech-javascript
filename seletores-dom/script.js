29821c8150d98c783979e3561be120ae7a56761a
Roberto Zanin <raszanin@gmail.com>
2022-12-12 14:30:29 -0300

    seletores dom

---
+++ seletores-dom/script.js
@@ -0,0 +1,13 @@
+console.log("Olá Mundo, Hello World, Ciao mondo, Hallo Welt");
+
+const titulo = document.getElementById("titulo");
+console.log(titulo);
+
+const textos = document.getElementsByClassName("texto-simples");
+console.log(textos);
+
+const segundoTitulo = document.querySelector("div h2");
+console.log(segundoTitulo);
+
+const elementos = document.querySelectorAll("h2");
+console.log(elementos);
